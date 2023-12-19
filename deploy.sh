427489d3ab37ff17f99251e89ca75494c292b1fd
wawan-ikhwan <ikhwanperwira@gmail.com>
2023-12-19 09:41:37 +0700

    memperbaiki deployer

--- deploy.sh
+++ deploy.sh
@@ -4,12 +4,12 @@
 git pull &&
 
 # Build Image
-pack build deepcare-image --builder gcr.io/buildpacks/builder:v1 --path . &&
+pack build deepcare_image --builder gcr.io/buildpacks/builder:v1 --path . &&
 
 # Push to GCR
 gcloud auth configure-docker &&
-docker tag deepcare_image:latest gcr.io/deep-care-capstone/deepcare-image:latest &&
-docker push gcr.io/deep-care-capstone/deepcare-image:latest &&
+docker tag deepcare_image:latest gcr.io/deep-care-capstone/deepcare_image:latest &&
+docker push gcr.io/deep-care-capstone/deepcare_image:latest &&
 
 # Deploy to CloudRun
-gcloud run deploy deepcare-service --image gcr.io/deep-care-capstone/deepcare-image:latest
+gcloud run deploy deepcare-api --image gcr.io/deep-care-capstone/deepcare_image:latest
