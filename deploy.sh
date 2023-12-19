25b7124af36b4ffb0ff474d9e481b924e8806f5e
wawan-ikhwan <ikhwanperwira@gmail.com>
2023-12-19 09:34:08 +0700

    deployer

---
+++ deploy.sh
@@ -0,0 +1,15 @@
+#/bin/bash
+
+# Update Repo
+git pull
+
+# Build Image
+pack build --builder gcr.io/buildpacks/builder:v1 --path ./deepcare_image
+
+# Push to GCR
+gcloud auth configure-docker
+docker tag deepcare_image:latest gcr.io/deep-care-capstone/deepcare-image:latest
+docker push gcr.io/deep-care-capstone/deepcare-image:latest
+
+# Deploy to CloudRun
+gcloud run deploy deepcare-service --image gcr.io/deep-care-capstone/deepcare-image:latest
